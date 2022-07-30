9510268214d8505c431f72fd0194e13d424b68d9
Abhinav Thapa <abhinavthapa1998@gmail.com>
2022-07-30 22:58:48 +0530

    Count using useReducer

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,25 @@
-import { useState } from "react";
+import { useState, useReducer } from "react";
 import "./styles.css";
 
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "increment":
+      return { count: state.count + 1 };
+    case "decrement":
+      return { count: state.count + 1 };
+    default:
+      throw new Error();
+  }
+};
+
+const initialState = {
+  userInput: "",
+  count: 0,
+  color: false
+};
+
 export default function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
   const [userInput, setUserInput] = useState("");
   const [count, setCount] = useState(0);
   const [color, setColor] = useState(false);
@@ -14,16 +32,17 @@ export default function App() {
       <div>{userInput}</div>
       <div>
         Count:{" "}
-        <button onClick={() => setCount((count) => count + 1)}>Count</button>
+        <button onClick={() => dispatch({ type: "increment" })}>Count</button>
       </div>
-      <div>{count}</div>
+      <div>{state.count}</div>
       <div>
         Color: <button onClick={() => setColor(!color)}>Color</button>
       </div>
       <div
         style={{
-          width: "50px",
-          height: "50px",
+          width: "100px",
+          height: "100px",
+          margin: "8px",
           background: color ? "red" : "yellow"
         }}
       ></div>

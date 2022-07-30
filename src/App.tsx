334fa869976ca751887125812e57000ac6e83b86
Abhinav Thapa <abhinavthapa1998@gmail.com>
2022-07-30 23:10:53 +0530

    Full useReducer

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,63 @@
-import { useState, useReducer } from "react";
+import { useReducer } from "react";
 import "./styles.css";
 
-const reducer = (state, action) => {
-  switch (action.type) {
-    case "increment":
-      return { count: state.count + 1 };
-    case "decrement":
-      return { count: state.count + 1 };
-    default:
-      throw new Error();
-  }
-};
+interface IState {
+  userInput: string;
+  count: number;
+  color: boolean;
+}
 
-const initialState = {
+interface IAction {
+  type: string;
+  payload: string | number | boolean;
+}
+
+const initialState: IState = {
   userInput: "",
   count: 0,
   color: false
 };
 
+const reducer = (state: IState, action: IAction) => {
+  switch (action.type) {
+    case "input":
+      return { ...state, userInput: action.payload };
+    case "increment":
+      return { ...state, count: state.count + 1 };
+    case "color":
+      return { ...state, color: !state.color };
+    default:
+      throw new Error();
+  }
+};
+
 export default function App() {
   const [state, dispatch] = useReducer(reducer, initialState);
-  const [userInput, setUserInput] = useState("");
-  const [count, setCount] = useState(0);
-  const [color, setColor] = useState(false);
   return (
     <div className="App">
       <div>
         Input:{" "}
-        <input onChange={(e) => setUserInput(e.target.value)} type="text" />
+        <input
+          onChange={(e) => dispatch({ type: "input", payload: e.target.value })}
+          type="text"
+        />
       </div>
-      <div>{userInput}</div>
+      <div>{state.userInput}</div>
       <div>
         Count:{" "}
         <button onClick={() => dispatch({ type: "increment" })}>Count</button>
       </div>
       <div>{state.count}</div>
       <div>
-        Color: <button onClick={() => setColor(!color)}>Color</button>
+        Color:{" "}
+        <button onClick={() => dispatch({ type: "color" })}>Color</button>
       </div>
       <div
         style={{
           width: "100px",
           height: "100px",
           margin: "8px",
-          background: color ? "red" : "yellow"
+          background: state.color ? "red" : "yellow"
         }}
       ></div>
     </div>
